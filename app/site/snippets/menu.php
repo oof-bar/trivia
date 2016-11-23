c174345de8f1da2283d5f88a6bbb84691b718843
Savannah Julian <savannah.julian@gmail.com>
2016-11-22 16:35:52 -0800

    New header styles

--- app/site/snippets/menu.php
+++ app/site/snippets/menu.php
@@ -1,11 +1,10 @@
 <section class="menu">
   <div class="wrap">
     <div class="nav">
-      <ul>
-        <li><a href="<?= page('home')->url() ?>">Dashboard</a></li>
-        <li><a href="<?= page('stats')->url() ?>">All Stats</a></li>
-        <li><a href="<?= page('about')->url() ?>">About</a></li>
-      </ul>
+      <a href="<?= page('home')->url() ?>" class="dashboard tab">Dashboard</a>
+      <a href="<?= page('stats')->url() ?>" class="stats tab">All Stats</a>
+      <a href="#" class="charts tab">Charts</a>
+      <a href="<?= page('about')->url() ?>" class="about tab">About</a>
     </div>
   </div>
 </section>
