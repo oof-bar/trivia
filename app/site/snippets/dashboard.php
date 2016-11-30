0133933183060b898b7535163d3c07f0fbcc6cb0
Savannah Julian <savannah.julian@gmail.com>
2016-11-30 13:54:12 -0800

    Moving type below numbers on dashboard

--- app/site/snippets/dashboard.php
+++ app/site/snippets/dashboard.php
@@ -2,28 +2,28 @@
   <div class="wrap">
     <div class="stats">
       <div class="stat">
+        <div class="data">50</div>
         <h3>Games Played</h3>
-        <div class="data">50</div>
       </div>
       <div class="stat">
+        <div class="data">50</div>
         <h3>Highest Score</h3>
-        <div class="data">50</div>
       </div>
       <div class="stat">
+        <div class="data">50</div>
         <h3>Times in Top 3</h3>
-        <div class="data">50</div>
       </div>
       <div class="stat">
+        <div class="data">50</div>
         <h3>Weeks Missed</h3>
-        <div class="data">50</div>
       </div>
       <div class="stat">
+        <div class="data">50</div>
         <h3>Average Score</h3>
-        <div class="data">50</div>
       </div>
       <div class="stat">
-        <h3>First Place</h3>
         <div class="data">50</div>
+        <h3>Games Won</h3>
       </div>
     </div>
   </div>
